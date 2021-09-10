a2fb0443acf4b1ad0e68fbbdc5d02f14fa5c8dfa
Athan Clark <athan.clark@gmail.com>
2021-09-09 20:53:50 -0400

    need to fix

--- src/Finance/Account.hs
+++ src/Finance/Account.hs
@@ -1,15 +1,30 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Finance.Account where
 
 import           Finance.Dollar  (Dollar, dollarPrinter)
 
 import           Control.DeepSeq (NFData)
+import Data.Foldable (foldlM)
 import           Data.Text       (Text)
 import qualified Data.Text       as T
+import Data.Maybe (isJust)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.String (IsString)
 import           GHC.Generics    (Generic)
 
+
+type Accounts = Map AccountId AccountAux
+
+data AccountAux = AccountAux
+  { accountAuxLimit :: AccountLimit
+  , accountAuxColor :: Text
+  , accountAuxBalance :: Dollar
+  } deriving (Show, Read, Eq, Ord, Generic)
+instance NFData AccountAux
 
 data AccountLimit
   = NoRestriction
@@ -18,23 +33,15 @@ data AccountLimit
   deriving (Show, Read, Eq, Ord, Enum, Bounded, Generic)
 instance NFData AccountLimit
 
--- | An account with a unique identifier and nickname
-data Account = Account
-  { accountName  :: Text -- ^ unique name
-  , accountLimit :: AccountLimit -- ^ limits for the account
-  , accountColor :: Text -- ^ hex color
-  } deriving (Show, Read, Generic)
-instance NFData Account
-instance Eq Account where
-  (Account x _ _) == (Account y _ _) = x == y
-instance Ord Account where
-  compare (Account x _ _) (Account y _ _) = compare x y
+-- | An account with a unique identifier
+newtype AccountId = AccountId {getAccountId :: Text}
+  deriving (Show, Read, Eq, Ord, Generic, NFData, IsString)
 
-blankAccount :: Account
-blankAccount = Account "" NoRestriction ""
+blankAccount :: (AccountId, AccountAux)
+blankAccount = ("", AccountAux NoRestriction "" 0)
 
-outOfLimitError :: Account -> Dollar -> Maybe Text
-outOfLimitError a@(Account name limit _) v = case limit of
+outOfLimitError :: AccountId -> AccountLimit -> Dollar -> Maybe Text
+outOfLimitError name limit v = case limit of
   NoRestriction -> Nothing
   OnlyPositive
     | v < 0 ->
@@ -45,7 +52,17 @@ outOfLimitError a@(Account name limit _) v = case limit of
       Just $ "Account " <> T.pack (show name) <> " is Only Negative but has value of $" <> dollarPrinter v
     | otherwise -> Nothing
 
-validate :: Account -> Dollar -> Maybe Text
-validate a@(Account name _ _) v
+validate :: AccountId -> AccountLimit -> Dollar -> Maybe Text
+validate name limit v
   | name == "" = Just "Account name can't be left blank"
-  | otherwise = outOfLimitError a v
+  | otherwise = outOfLimitError name limit v
+
+addAccount :: Accounts -> AccountId -> AccountAux -> Maybe Accounts
+addAccount acc name aux@(AccountAux limit _ v)
+  | isJust (validate name limit v) = Nothing
+  | Map.null (Map.filterWithKey (\a' _ -> a' == name) acc) =
+      pure $ Map.insert name aux acc
+  | otherwise = Nothing
+
+mkAccounts :: [(AccountId, AccountAux)] -> Maybe Accounts
+mkAccounts = foldlM (uncurry . addAccount) Map.empty
